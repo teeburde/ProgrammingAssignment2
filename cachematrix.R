6496d5fecbf1652dfe28bcf86fefbdcef01c5ec8
teeburde <89220060+teeburde@users.noreply.github.com>
2021-08-19 17:03:44 -0400

    Update cachematrix.R
    I had issues with the use of "<-" and "=". I tried to lookup the difference but it still isn't quite clear. I was able to run cacheSolve(x) sucessfully at the end of this (with a sample matrix of size [3,3])

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Referencing the inital makeVector format 
+## combined with the cache. We will use "inverse" in place
+## of "mean" for the both function, as it follows similar
+## framework.
 
-## Write a short comment describing this function
+## The following function creates a specific Matrix that
+## can cache its inverse, per input matrix (x).
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL  #m for matrix
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinvm = function(invm) m <<- invm
+  getinvm = function() m
+  list(set = set, get = get,
+       setinvm = setinvm,
+       getinvm = getinvm)
 }
 
 
-## Write a short comment describing this function
+## This function determines the inverse of matrix (x) derived from
+## the above equation and stores it in the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m = x$getinvm()   #x in terms of inverse matrix
+  if(!is.null(m)) { #if done will instantly return, if not will move on to get data
+    message("getting cached data")
+    return(m)
+    
+  }
+  
+  data = x$get()
+  m = solve(data, ...)
+  x$setinvm(m)
+  
 }
